c3871b8906a8a5b0377f13e08ce5c9be9b046567
Eden Yang <dexuan90213@gmail.com>
2020-03-13 17:07:41 +0800

    os module show memory info

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const path = require('path')
-
 let pathObj = path.parse(__filename)
 
 console.log(pathObj)
+
+//
+
+const os = require('os')
+let totalMemory = os.totalmem()
+let freeMemory = os.freemem()
+
+console.log(`Total Memory: ${totalMemory}`)
+console.log(`Free Memory: ${freeMemory}`)
