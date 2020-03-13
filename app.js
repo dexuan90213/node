623bfaf71720c0ebce41316a155c0d2f33636f15
Eden Yang <dexuan90213@gmail.com>
2020-03-13 17:21:01 +0800

    fs module read file

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ let freeMemory = os.freemem()
 
 console.log(`Total Memory: ${totalMemory}`)
 console.log(`Free Memory: ${freeMemory}`)
+
+//
+
+const fs = require('fs')
+
+const files = fs.readdirSync('./')
+console.log(files)
+
+fs.readdir('./', function(err, files) {
+  if (err) {
+    console.log('Error', err)
+  } else {
+    console.log('Result', files)
+  }
+})
